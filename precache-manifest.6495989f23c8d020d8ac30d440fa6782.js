4e861e266fe204efb4f49c917cf1ec58752183a6
Vitalijs Litvinovs <vitalijs@gerlionti.com>
2022-10-02 17:30:11 +0300

    Updates

---
+++ precache-manifest.6495989f23c8d020d8ac30d440fa6782.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9537ea0d36b61f09cc20c10cb4e2fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69c564df32ccb59c9336",
+    "url": "/static/css/main.8cdcfd3f.chunk.css"
+  },
+  {
+    "revision": "98422bcd2ecc4e15b1ff",
+    "url": "/static/js/2.58350118.chunk.js"
+  },
+  {
+    "revision": "69c564df32ccb59c9336",
+    "url": "/static/js/main.227820a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
